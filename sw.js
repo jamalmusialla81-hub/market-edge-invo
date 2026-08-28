f367abe66561c4d6488b2ac72ed20ad96515fd91
Jakob Lal <jamalmusialla81@gmail.com@gmail.com>
2026-08-28 13:56:27 +1000

    Bust Market Edge cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'market-edge-v7';
+const CACHE = 'market-edge-v8';
 const APP_SHELL = ['./', './index.html', './market-edge.html', './quant-engine.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
